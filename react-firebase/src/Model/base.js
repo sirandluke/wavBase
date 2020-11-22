1a29d4fc3231db0d98db8aaadef64444c3aaaacd
Luke Sirand <lsirand@ucsd.edu>
2020-11-22 15:22:32 -0800

    Update base.js

--- react-firebase/src/Model/base.js
+++ react-firebase/src/Model/base.js
@@ -9,7 +9,7 @@ const db = firebase.initializeApp({
     authDomain: "wavbasedb-9a679.firebaseapp.com",
     databaseURL: "https://wavbasedb-9a679.firebaseio.com/",
     projectId: "wavbasedb-9a679",
-    storageBucket: "wavbasedb-9a679.appspot.com",
+    storageBucket: "wavbasedb-9a679.appspot.com",  // Path to Firebase Storage.
     messagingSenderId: "707190956098",
     appId: "1:707190956098:web:88fa31bd96d13289c64ddb",
     measurementId: "G-C56V3L638G"
