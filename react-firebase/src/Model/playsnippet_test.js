48ee50b650a1dd38a93ca3ea716e97c1fbc20fa1
MGalaxy1984 <zhxie@ucsd.edu>
2020-11-22 19:24:10 -0800

    Create playsnippet_test.js

---
+++ react-firebase/src/Model/playsnippet_test.js
@@ -0,0 +1,22 @@
+//import logo from './logo.svg';
+import './App.css';
+import React from "react";
+import {pauseAudio, playAudio, stopAudio} from "./playsnippet";
+
+function App() {
+
+    const audioSource= 'test1.wav';
+    function handlePlay() {
+        playAudio(audioSource);
+    }
+
+  return (
+    <div>
+        <button onClick={handlePlay}>Play</button>
+        <button onClick={pauseAudio}>Pause</button>
+        <button onClick={stopAudio}>Stop</button>
+    </div>
+  );
+}
+
+export default App;
