5ee7bd7690bc48cc949a9ac4455cfa260e733ad4
MGalaxy1984 <zhxie@ucsd.edu>
2020-11-30 22:24:09 -0800

    Update Repository.js

--- react-firebase/src/View/routes/Repository.js
+++ react-firebase/src/View/routes/Repository.js
@@ -1,26 +1,28 @@
-let audio;
 
-export function playAudio(source) {
-    if (audio == null) {
-        audio = new Audio(source);
-    }
-    audio.play();
-}
-
-export function pauseAudio() {
-    audio.pause();
-}
-
-export function stopAudio() {
-    audio.pause();
-    audio.currentTime = 0;
-}
-
-function playsnippet() {
+const repository = ({ history }) => {
+    let audio;
     const audioSource= 'https://firebasestorage.googleapis.com/v0/b/wavbasedb-9a679.appspot.com/o/test_audio%2Ftest_piano.mp3?alt=media&token=e3dce63f-0aab-4d68-be39-39893c759e8e';
+
+    export function playAudio(source) {
+        if (audio == null) {
+            audio = new Audio(source);
+        }
+        audio.play();
+    }
+
+    export function pauseAudio() {
+        audio.pause();
+    }
+
+    export function stopAudio() {
+        audio.pause();
+        audio.currentTime = 0;
+    }
+
     function handlePlay() {
         playAudio(audioSource);
     }
+
 
     return (
         <div>
@@ -30,3 +32,6 @@ function playsnippet() {
         </div>
     );
 }
+
+
+export default repository;
