752b6cab5f529bf44ddd439bd97878dab12be2a8
Luke Sirand <lsirand@ucsd.edu>
2020-12-01 00:20:45 -0800

    ReactPlayer attempt to for play snippet use case

---
+++ react-firebase/src/Model/PlayButton.js
@@ -0,0 +1,46 @@
+import '../App.css';
+import React, { useState } from "react";
+import db from "./base";
+import ReactPlayer from "react-player";
+import * as K from "../Constants";
+
+const PlayButton = (props) => {
+    const [audio, loadAudio] = useState("");
+    const storage= db.storage();
+    const storageRef = storage.ref();
+    const audioLoader = info => {
+        storageRef.child('test_audio/test_piano.mp3').getDownloadURL().then( function (url) {
+
+            // Download audio file directly
+            let xhr = new XMLHttpRequest();
+            xhr.responseType = 'blob';
+            xhr.onload = function (event) {
+                let blob = xhr.response;
+                let objURL = URL.createObjectURL(blob)
+            }
+            xhr.open('GET', url);
+            xhr.send();
+            loadAudio(url)
+            console.log(url)
+        }).catch((error) => {
+            //console.log(error);
+            alert(K.unknown_err);
+        })
+      }
+        return (
+            <div>
+                <ReactPlayer
+                    url={loadAudio}
+                    width="400px"
+                    height="50px"
+                    playing={false}
+                    controls={true}
+                    volume={1}
+                    progressInterval={5000}
+                    pip={true}
+                />
+            </div>
+        );
+}
+
+export default PlayButton;
