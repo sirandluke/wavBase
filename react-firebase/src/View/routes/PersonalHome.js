dafe897b7b89169e93f7ca186a64a578d8e88082
MGalaxy1984 <zhxie@ucsd.edu>
2020-12-01 06:23:07 -0800

    Profile Page added

--- react-firebase/src/View/routes/PersonalHome.js
+++ react-firebase/src/View/routes/PersonalHome.js
@@ -14,7 +14,14 @@ const PersonalHome = ({history}) => {
     //let name, email, photoUrl, uid, emailVerified;
     let name = "YYY";
     if (user != null) {
-        name = user.displayName;
+        //name = user.email;
+        let username;
+        let uid = db.auth().currentUser.uid;
+        let firebaseRef = db.database().ref('users/' + uid);
+        firebaseRef.on('value', (snapshot) =>{
+            username = snapshot.val().username;
+        })
+        name = username;
     }
 
     const redirectCreateRepo = () => {
@@ -25,25 +32,28 @@ const PersonalHome = ({history}) => {
         history.push("/repository");
     }
 
+    const redirectProfile = () => {
+        history.push("/profile");
+    }
+
+
 
     return (
         <div className="container">
             <h1>Home</h1>
             <div className="nav">
                 <h2>wavBase</h2>
-                <img src={logo} alt="wavBase Logo" width="50" height="50" />
+                <img src={logo} alt="wavBase Logo" width="50" height="50"/>
                 <h2>Hello {name}</h2>
-                    <DropdownButton id="dropdown-basic-button" title="Dropdown button">
-                        <Dropdown.Item href="#/action-1">Action</Dropdown.Item>
-                        <Dropdown.Item href="#/action-2">Another action</Dropdown.Item>
-                        <Dropdown.Item href="#/action-3">Something else</Dropdown.Item>
-                    </DropdownButton>
+                <DropdownButton id="dropdown-basic-button" title="User">
+                    <Dropdown.Item as="button" onClick={redirectProfile}>My Profile</Dropdown.Item>
+                    <Dropdown.Item as="button" onClick={redirectRepo}>My Repositories</Dropdown.Item>
+                    <Dropdown.Item as="button" onClick={() => db.auth().signOut()}>Sign Out</Dropdown.Item>
+                </DropdownButton>
                 <ul>
                     <button onClick={redirectCreateRepo}>Create Repository</button>
-                    <button onClick={redirectRepo}>TestRepositories</button>
                     <li>Repositories</li>
                     <li>Likes</li>
-                    <button onClick={() => db.auth().signOut()}>Sign Out</button>
                 </ul>
             </div>
         </div>
--- react-firebase/src/View/routes/Repository.js
+++ react-firebase/src/View/routes/Repository.js
@@ -7,7 +7,7 @@ import {PlayButton} from "../../Model/PlayButton";
 const Repository = ({ history }) => {
     let audioSource= 'https://firebasestorage.googleapis.com/v0/b/wavbasedb-9a679.appspot.com/o/test_audio%2Ftest_piano.mp3?alt=media&token=e3dce63f-0aab-4d68-be39-39893c759e8e';
 
-    function redirectHome() {
+    const redirectHome = () => {
         history.push("/");
     }
 
