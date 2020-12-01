dae685516754899e47cee8e5012f6557b06b0761
MGalaxy1984 <zhxie@ucsd.edu>
2020-12-01 01:39:21 -0800

    Update Repository.js

--- react-firebase/src/View/routes/Repository.js
+++ react-firebase/src/View/routes/Repository.js
@@ -5,30 +5,9 @@ import * as K from '../../Constants';
 import * as FirebaseHandler from  "../../Model/FirebaseHandler.js";
 import {PlayButton} from "../../Model/PlayButton";
 
-export class playbutton extends React.Component {
-    constructor() {
-        super();
-        this.state = {isPlaying: false};
-        this.handleClick = this.handleClick.bind(this);
-    }
-
-    handleClick() {
-        this.setState(prevState => ({
-            isPlaying: !prevState.Playing
-        }));
-
-    }
-
-    render() {
-        return (
-            <button onClick={this.handleClick}>{this.state.isPlaying ? 'Play' : 'Pause'}</button>
-        );
-    }
-}
-
 const Repository = ({ history }) => {
     let audio;
-    const audioSource= 'https://firebasestorage.googleapis.com/v0/b/wavbasedb-9a679.appspot.com/o/test_audio%2Ftest_piano.mp3?alt=media&token=e3dce63f-0aab-4d68-be39-39893c759e8e';
+    let audioSource= 'https://firebasestorage.googleapis.com/v0/b/wavbasedb-9a679.appspot.com/o/test_audio%2Ftest_piano.mp3?alt=media&token=e3dce63f-0aab-4d68-be39-39893c759e8e';
 
     function playAudio(source) {
         if (audio == null) {
@@ -61,6 +40,7 @@ const Repository = ({ history }) => {
             <button onClick={pauseAudio}>Pause</button>
             <button onClick={stopAudio}>Stop</button>
             < PlayButton audio={audioSource} />
+            <button onClick={redirectHome}>Go Back to Home!</button>
         </div>
     );
 };
