99d095b4a5266b3a8604da46b90f69547e9b9520
MGalaxy1984 <zhxie@ucsd.edu>
2020-12-02 05:01:55 -0800

    individual repository really worked now

--- react-firebase/src/View/routes/IndividualRepository.js
+++ react-firebase/src/View/routes/IndividualRepository.js
@@ -4,12 +4,25 @@ import * as K from '../../Constants';
 import * as FirebaseHandler from  "../../Model/FirebaseHandler.js";
 import {PlayButton} from "../../Model/PlayButton";
 import {audioSource} from "./Repository";
+import db from "../../Model/base";
 
 const IndividualRepository = (repo_id) => {
 
+    let repo_ref = db.database().ref().child('repositories/' + repo_id);
+    let name, description;
+    repo_ref.on('value', (snapshot) => {
+        name = snapshot.val().name;
+        description = snapshot.val().description;
+    });
+
     return (
         <div>
-            <h1>{repo_id}</h1>
+            <h1>{name}</h1>
+            <p>{description}</p>
+            <ul>
+                <li>Snapshot 1</li>
+                <li>Snapshot 2</li>
+            </ul>
             <PlayButton audio={audioSource}/>
         </div>
     );
--- react-firebase/src/View/routes/Repository.js
+++ react-firebase/src/View/routes/Repository.js
@@ -10,23 +10,9 @@ import IndividualRepository from "./IndividualRepository";
 export const audioSource = 'https://firebasestorage.googleapis.com/v0/b/wavbasedb-9a679.appspot.com/o/test_audio%2Ftest_piano.mp3?alt=media&token=e3dce63f-0aab-4d68-be39-39893c759e8e';
 const Repository = () => {
 
-    let user = db.auth().currentUser;
-    let name, profile_picture_path, user_email;
-    let uid;
-    if (user != null) {
-        let username;
-        uid = db.auth().currentUser.uid;
-        let userRef = db.database().ref('users/' + uid);
-        userRef.on('value', (snapshot) => {
-            username = snapshot.val().username;
-            profile_picture_path = snapshot.val().profile_picture;
-            user_email = snapshot.val().email;
-        })
-        name = username;
-    }
-
     let repo_links = [];
     let repo_paths = [];
+    const uid = db.auth().currentUser.uid;
     let repo_ref = db.database().ref().child('repositories');
     repo_ref.orderByChild('user_id').equalTo(uid).on('value', (snapshot) => {
         snapshot.forEach((entry) => {
