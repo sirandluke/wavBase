8cecaec9708849b25740e6e02bc0017ae292c0d9
Luke Sirand <lsirand@ucsd.edu>
2020-12-11 16:19:37 -0800

    fixed port

--- server/server.js
+++ server/server.js
@@ -20,12 +20,12 @@ app.use(bodyParser.urlencoded({extended: false}));
 app.use(bodyParser.json());
 
 
-app.use('/', require('../src/index'));
+app.use('/', require('../index'));
 
 app.use('/', require('../src/Controller/Controller'));
 
 
-const PORT = (process.env.PORT || 5000);
+const PORT = (process.env.PORT || 8000);
 console.log(PORT)
 app.listen(PORT, () =>
     console.log(`Server started on port ${PORT}`)
